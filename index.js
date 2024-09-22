069895a738123a4d0395c9f02d9c413d054ee0c8
BALAVIGNESHWARN.P <137701826+BALAVIGNESHWARAN23@users.noreply.github.com>
2024-09-23 00:41:03 +0530

    update

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ app.post("/sign_up", async (req, res) => {
 
         await user.save();
         console.log("Record Inserted Successfully");
-        return res.redirect('https://lexmoon.onrender.com');
+        return res.redirect('https://lexmoon.onrender.com/#home');
     } catch (err) {
         console.error(err);
        
@@ -155,7 +155,7 @@ app.post("/login", async (req, res) => {
                 console.error(err);
                 return res.redirect('/loginerror.html');
             }
-            return res.redirect('https://lexmoon.onrender.com');
+            return res.redirect('https://lexmoon.onrender.com/#home');
         });
     } catch (err) {
         console.error(err);
@@ -271,11 +271,11 @@ app.post("/reset_password", async (req, res) => {
 app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
 app.get("/auth/google/callback", passport.authenticate("google", { failureRedirect: "/loginerror.html" }), (req, res) => {
-    res.redirect("https://lexmoon.onrender.com");
+    res.redirect("https://lexmoon.onrender.com/#home");
 });
 
 
-app.get("https://lexmoon.onrender.com", ensureAuthenticated, (req, res) => {
+app.get("https://lexmoon.onrender.com/#home", ensureAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'indexbg.html'));
 });
 
@@ -314,7 +314,7 @@ async function checkUserEmail(req, res) {
 
             if (user) {
                
-                return res.redirect('https://lexmoon.onrender.com');
+                return res.redirect('https://lexmoon.onrender.com/#home');
             } else {
            
                 return res.redirect('/indexbg.html');
@@ -323,10 +323,13 @@ async function checkUserEmail(req, res) {
             console.error("Error in checking email in MongoDB:", err);
             return res.redirect('/indexbg.html'); 
         }
-    } 
+    } else {
+        
+        return res.redirect('/indexbg.html');
+    }
 }
 
 app.get("/indexbg.html", checkUserEmail, (req, res) => {
    
-     return res.redirect('https://lexmoon.onrender.com');
+    res.sendFile(path.join(__dirname, 'public', 'indexbg.html'));
 });
