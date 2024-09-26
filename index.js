9ffba2fa01ca32b93c95d57165ebc0079c9344a7
BALAVIGNESHWARN.P <137701826+BALAVIGNESHWARAN23@users.noreply.github.com>
2024-09-26 22:05:56 +0530

    update

--- index.js
+++ index.js
@@ -61,7 +61,8 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL:  "https://https://www.lexmoon.com.onrender.com/auth/google/callback"
+
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await User.findOne({ googleId: profile.id });
