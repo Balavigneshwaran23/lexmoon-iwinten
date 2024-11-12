e3c29eb96ab6b22bce23e00b305e28c3c440be93
BALAVIGNESHWARN.P <137701826+BALAVIGNESHWARAN23@users.noreply.github.com>
2024-11-12 21:20:18 +0530

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dotenv = require("dotenv");
 const path = require("path");
 
 dotenv.config();
-require("./config/passport-setup"); // Ensure this path is correct
+require("./config/passport-setup"); 
 
 const authRoutes = require("./routes/authRoutes");
 const passwordRoutes = require("./routes/passwordRoutes");
@@ -26,6 +26,22 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+
+// Set up EJS as the view engine
+app.set("view engine", "ejs");
+
+// Set the views folder
+app.set("views", path.join(__dirname, "views"));
+
+// Global middleware for setting meta tags
+app.use((req, res, next) => {
+    res.locals.meta = {
+        description: "Discover our law-based translator specializing in English to Tamil translation. We provide accurate translations for legal documents, ensuring precise legal terminology and compliance with legal standards.",
+        keywords: "law, legal translator, translation services, English to Tamil translation, legal documents translation, law-based translator, Tamil legal terminology, bilingual legal services"
+    };
+    next();
+});
+
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Connected to Database"))
@@ -38,10 +54,7 @@ app.use("/", passwordRoutes);
 // Route for the homepage
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "indexbg.html"));
-    res.render('index', {
-        keywords: "law, legal translator, translation services, English to Tamil translation, legal documents translation, law-based translator, Tamil legal terminology, bilingual legal services",
-        description: "Discover our law-based translator specializing in English to Tamil translation. We provide accurate translations for legal documents, ensuring precise legal terminology and compliance with legal standards."
-    });
+
 
 });
 
